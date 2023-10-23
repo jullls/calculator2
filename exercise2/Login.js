20f46181d3af49a445b6237fbeb026320a1aa578
jullls <s2200461@student.unklab.ac.id>
2023-10-23 18:58:20 +0800

    new

---
+++ exercise2/Login.js
@@ -0,0 +1,15 @@
+// create button login
+function LoginButton (props) {
+return (
+<button onClick={props.onClick}>
+Login
+</button>
+);
+}
+
+function LogoutButton (props) {
+return (
+<button onClick={props.onClick}>
+Logout </button>
+);
+}
